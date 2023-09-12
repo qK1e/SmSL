4b842a2fa874222bbb4215d8a357da97a5f813de
Pavlovich <illia.potapov.wrk@gmail.com>
2023-09-13 00:23:19 +0700

    удалил тестер парсера;вынес символьные таблицы за пределы языка

--- index.js
+++ index.js
@@ -4,17 +4,62 @@ import { indentWithTab } from "@codemirror/commands"
 import { keymap } from '@codemirror/view'
 import TreeVisualizer from "./src/Visualisation/TreeVisualizer"
 
+let symbolTable = {
+  clients: {
+    full_name: "string",
+    first_name: "string",
+    middle_name: "string",
+    last_name: "string",
+    age: "number",
+    city: "string",
+    orders: {
+      status: "string",
+      items: {
+        quantity: "number",
+        product: {
+          category: {
+            identifier: "string",
+            name: "string"
+          },
+          price: "number",
+          name: "string"
+        }
+      }
+    },
+    contacts: {
+      type: "string",
+      value: "string",
+      validation_code: "number",
+      validation_date: "datetime"
+    },
+    communications: {
+      type: "string",
+      events: {
+        type: "string",
+        date: "datetime"
+      },
+      id: "number"
+    },
+    channels: {
+      type: "string",
+      subscribed: "boolean"
+    },
+    mailing_categories: {
+      name: "string",
+      subscribed: "boolean"
+    }
+  }
+}
+
 let ev = new EditorView({
   doc: "",
   extensions: [
     basicSetup, 
-    smsl(),
+    smsl(symbolTable),
     keymap.of([indentWithTab])
   ],
   parent: document.getElementById('editor')
 });
-
-
 
 window.editor = ev;
 
--- src/language.js
+++ src/language.js
@@ -2,54 +2,8 @@ import {parser} from "./parser.js"
 import autocompletion from './Autocompletion/autocompletion'
 import {LanguageSupport, LRLanguage, indentNodeProp, delimitedIndent} from "@codemirror/language"
 
-let symbolTable = {
-  clients: {
-    full_name: "string",
-    first_name: "string",
-    middle_name: "string",
-    last_name: "string",
-    age: "number",
-    city: "string",
-    orders: {
-      status: "string",
-      items: {
-        quantity: "number",
-        product: {
-          category: {
-            identifier: "string",
-            name: "string"
-          },
-          price: "number",
-          name: "string"
-        }
-      }
-    },
-    contacts: {
-      type: "string",
-      value: "string",
-      validation_code: "number",
-      validation_date: "datetime"
-    },
-    communications: {
-      type: "string",
-      events: {
-        type: "string",
-        date: "datetime"
-      },
-      id: "number"
-    },
-    channels: {
-      type: "string",
-      subscribed: "boolean"
-    },
-    mailing_categories: {
-      name: "string",
-      subscribed: "boolean"
-    }
-  }
-}
-
-export const language = LRLanguage.define({
+export let smsl = (symbolTable) => {
+  const language = LRLanguage.define({
     parser: parser.configure({
       props: [
         indentNodeProp.add({
@@ -62,10 +16,7 @@ export const language = LRLanguage.define({
       autocomplete: autocompletion,
       symbolTable: symbolTable
     }
-})
+  });
 
-export let languageSupport = new LanguageSupport(language);
-
-export let smsl = () => {
   return new LanguageSupport(language);
 }
